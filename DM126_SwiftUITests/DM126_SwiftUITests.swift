1c0b1d690a063b56d39d82323fbcafd95a031bf8
psyeatos <joaogml1@gmail.com>
2024-11-09 11:24:04 -0500

    Initial Commit

---
+++ DM126_SwiftUITests/DM126_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  DM126_SwiftUITests.swift
+//  DM126_SwiftUITests
+//
+//  Created by user270465 on 11/9/24.
+//
+
+import Testing
+@testable import DM126_SwiftUI
+
+struct DM126_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
