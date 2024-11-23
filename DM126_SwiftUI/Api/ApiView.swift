b41f758ddf43169f6c66b37677a139c57fafca77
psyeatos <joaogml1@gmail.com>
2024-11-23 12:40:21 -0500

    - PokeApi - aula 4

---
+++ DM126_SwiftUI/Api/ApiView.swift
@@ -0,0 +1,18 @@
+//
+//  ApiView.swift
+//  DM126_SwiftUI
+//
+//  Created by user270465 on 11/23/24.
+//
+
+import SwiftUI
+
+struct ApiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ApiView()
+}
